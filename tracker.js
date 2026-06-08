a1bac492d7a8c6ed5595017d3756af4f519685e9
unknown <slmgilleyyt@gmail.com>
2026-06-07 23:07:58 -0400

    main

---
+++ tracker.js
@@ -0,0 +1,8 @@
+<script>
+  window._rootfoundry = {
+    dashboardUrl: 'https://root-foundry.com',
+    siteId: 'f4532f88661d56d9c4db2f3a',
+    userId: 'jSQnZBWuaMcR5Vr1WFRyuFE9uZp2'
+  };
+</script>
+<script src="https://root-foundry.com/tracking/tracker.js"></script>
