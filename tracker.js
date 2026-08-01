e09a833aadedb3401cf8a0d7caed458ca7f404be
unknown <slmgilleyyt@gmail.com>
2026-08-01 11:40:57 -0400

    main

--- tracker.js
+++
@@ -1,8 +0,0 @@
-<script>
-  window._rootfoundry = {
-    dashboardUrl: 'https://root-foundry.com',
-    siteId: 'f4532f88661d56d9c4db2f3a',
-    userId: 'jSQnZBWuaMcR5Vr1WFRyuFE9uZp2'
-  };
-</script>
-<script src="https://root-foundry.com/tracking/tracker.js"></script>
